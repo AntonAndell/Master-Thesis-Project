2d1e7c4592595cf108bc75e89e9dca7cd6ef00d4
Anton Andell <Anton@andell.eu>
2021-02-16 14:01:08 +0100

    graphing

---
+++ TUF/graphMessages/plott1.py
@@ -0,0 +1,61 @@
+# importing the required module 
+import matplotlib.pyplot as plt 
+import math
+from iota import Iota, TryteString, Address, Tag, ProposedTransaction
+from pprint import pprint
+import json
+
+TUFdata = open("TUFTarget1.json", "r")
+TUFdata = TUFdata.read()
+TUFdata= json.loads(TUFdata)
+filedata = TUFdata["signed"]["targets"]["file.txt"]
+xTUF = [] 
+# corresponding y axis values 
+yTUF = [] 
+for i in range(1,31):
+    yTUF += [len(json.dumps(TUFdata).encode("utf-8"))]
+    TUFdata["signed"]["targets"]["file"+str(i)+".txt"] = filedata
+    xTUF += [i]
+
+# x axis values 
+
+
+TUFTdata = open("Target1.json", "r")
+TUFTdata = TUFTdata.read()
+TUFTdata= json.loads(TUFTdata)
+x2TUFT = [] 
+# corresponding y axis values 
+y2TUFT = [] 
+for i in range(1,31):
+
+    tstring = TryteString.from_unicode(json.dumps(TUFTdata))
+    trytes = math.ceil(len(tstring)/2187)*2673
+    y2TUFT += [(trytes*math.log(3)*3)/(math.log(2)*8)]
+    TUFTdata["file"+str(i)+".txt"] = filedata
+    x2TUFT += [i]
+
+size = len(json.dumps(TUFTdata).encode("utf-8"))
+
+
+
+xTUFT =list(range(1,31))  
+# corresponding y axis values 
+yTUFT = [1588.726786660369]*30
+
+plt.plot(xTUFT, yTUFT, label="TUFT updating 1 target file")
+
+plt.plot(x2TUFT, y2TUFT, label="TUFT updating all target files")
+# plotting the points  
+plt.plot(xTUF, yTUF, label="TUF") 
+
+# naming the x axis 
+plt.xlabel('Number of Target Files') 
+# naming the y axis 2
+plt.ylabel('Data Downloaded') 
+  
+# giving a title to my graph 
+plt.title('Target download size') 
+  
+# function to show the plot 
+plt.legend()
+plt.show() 
---
+++ TUF/graphMessages/plott3.py
@@ -0,0 +1,42 @@
+# importing the required module 
+import matplotlib.pyplot as plt 
+import math
+import numpy as np
+from iota import Iota, TryteString, Address, Tag, ProposedTransaction
+from pprint import pprint
+import json
+
+TUFdata = open("TUFRoot.json", "r")
+TUFdata = TUFdata.read()
+TUFdata= json.loads(TUFdata)
+
+xTUF = [] 
+# corresponding y axis values 
+yTUF = [] 
+yTUF = np.array([len(json.dumps(TUFdata).encode("utf-8"))]) * np.array(list(range(1,31))) /1000
+print(yTUF)
+
+# x axis values 
+
+
+
+xTUFT =list(range(1,31))  
+# corresponding y axis values 
+yTUFT = np.array([1588.726786660369]*30)*np.array(xTUFT)/1000
+
+plt.plot(xTUFT, yTUFT, label="TUFT")
+
+
+plt.plot(xTUFT, yTUF, label="TUF") 
+
+# naming the x axis 
+plt.xlabel('Number of Root updates') 
+# naming the y axis 2
+plt.ylabel('Kilobytes') 
+  
+# giving a title to my graph 
+plt.title('Full Root download size') 
+  
+# function to show the plot 
+plt.legend()
+plt.show() 
